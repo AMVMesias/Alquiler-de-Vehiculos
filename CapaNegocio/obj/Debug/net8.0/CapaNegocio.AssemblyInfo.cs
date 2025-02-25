e7e4ccc8148a995dbbce0005e5d4dbdde2fbc489
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2025-02-25 17:48:57 -0500

    Carpetas - Config NuGet
    Se crean las carpetas de Entidad, Datos y Negocios así como se carga los paquetes Nuget Microsoft.EntityFrameworkCore

--- CapaNegocio/obj/Debug/net8.0/CapaNegocio.AssemblyInfo.cs
+++ CapaNegocio/obj/Debug/net8.0/CapaNegocio.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Alquiler")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CapaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6a2b07a42a726fd2129642210b0623edf5ff56")]
-[assembly: System.Reflection.AssemblyProductAttribute("Alquiler")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Alquiler")]
+[assembly: System.Reflection.AssemblyProductAttribute("CapaNegocio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CapaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
