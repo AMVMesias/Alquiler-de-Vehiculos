46e6eb03b1c67c07844e952314160bc041ce15cd
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2025-03-02 00:03:12 -0500

    update

--- Alquiler/obj/Debug/net8.0/Alquiler.AssemblyInfo.cs
+++ Alquiler/obj/Debug/net8.0/Alquiler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alquiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3f3d7b97dcb1fd70d34593d14028336299536e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53956c240f78f16749d6df10b23af52403a5e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alquiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alquiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CapaDatos/obj/Debug/net8.0/CapaDatos.AssemblyInfo.cs
+++ CapaDatos/obj/Debug/net8.0/CapaDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CapaDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb837f374a9544c436b6432421e68e0dd04778a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53956c240f78f16749d6df10b23af52403a5e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CapaDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CapaDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
